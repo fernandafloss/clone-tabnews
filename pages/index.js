690b37f9c06778e669c49768f927a135bc541691
Fernanda Ferreira <104260523+fernandafloss@users.noreply.github.com>
2024-07-03 23:02:33 +0000

    mensagem oficial

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,6 @@
 function Home (){
-    return <div>
-        <h1>teste1</h1>
-        <h2>teste 2</h2>
-        <h3>teste 3</h3>
-        <h4>teste 4</h4>
-        <h5>teste 5</h5>
-    </div>
-    
-
+    return <h1>Te amo meu benzinho S2</h1>
+        
 }
 
 export default Home;
