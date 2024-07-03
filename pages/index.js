e7a0ef5435c87fe19ea560d13611972638cf4088
Fernanda Ferreira <104260523+fernandafloss@users.noreply.github.com>
2024-07-03 22:52:38 +0000

    teste de tags html

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
 function Home (){
-    return <h1>Amor da minha vida</h1>
+    return <div>
+        <h1>teste1</h1>
+        <h2>teste 2</h2>
+        <h3>teste 3</h3>
+        <h4>teste 4</h4>
+        <h5>teste 5</h5>
+    </div>
+    
+
 }
 
 export default Home;
