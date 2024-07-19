5cdb62653fc60f74618643147ef8d67b9aae5848
Fernanda Ferreira <104260523+fernandafloss@users.noreply.github.com>
2024-07-19 13:55:42 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Projeto do curso.dev para treinar habilidades de desenvolvimento full stack de
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,14 @@
-function Home (){
-    return <h1>Moni, esse é meu primeiro site, e queria te dizer que eu te amo muito e estou muito orulhosa de ti.</h1>
-        
+function Home() {
+  return (
+    <h1>
+      Moni, esse é meu primeiro site, e queria te dizer que eu te amo muito e
+      estou muito orulhosa de ti.
+    </h1>
+  );
+}
+
+function teste() {
+  console.log("identação errada");
 }
 
 export default Home;
